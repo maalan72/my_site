0177945e501d6a24d349bc4403c57ecdcb554978
maalan72 <96998132+maalan72@users.noreply.github.com>
2022-01-03 01:43:11 +0500

    первая загрузка

---
+++ script.js
@@ -0,0 +1,7 @@
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+
+    page.classList.add('dark-theme');
+   // page.classList.remove('dark-theme');
+  };
